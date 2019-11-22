b996c9b02309e6c1c49d48b068bd38b019418492
elijah03ojo <44117114+elijah03ojo@users.noreply.github.com>
2019-11-22 11:40:51 -0500

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -1,6 +1,7 @@
 const readlineSync = require("readline-sync");
 let reversed = "";
 let integer = 0
+console.log()
 do{
   number = Number(readlineSync.question("Positive Integer: "));
 } while (number < Number.MIN_SAFE_INTEGER || number > Number.MAX_SAFE_INTEGER || Number.isNaN(number) || number % 1 != 0 || number <= 0)
@@ -14,4 +15,4 @@ do {
 
 reversed = reversed + number + "."
 
-console.log(reversed);
+console.log("\n" + reversed);;
