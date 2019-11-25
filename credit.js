883162a221cdb2f80fcd74360dd4e6bf60f8b4ab
elijah03ojo <44117114+elijah03ojo@users.noreply.github.com>
2019-11-24 19:20:09 -0500

    Update credit.js

--- credit.js
+++ credit.js
@@ -1,27 +1,54 @@
 const readlineSync = require("readline-sync");
-var creditCard
-var sum = 0
-var creditLength
+let counter = 1
+let creditNumber = 0
+let addition = 0
+let creditNumber2 = 0
+let sum = 0
+let digit1 = 0
+let digit2 = 0
+let singularDigit = 0
+let digit = 0
 console.log();
 
 do {
-    creditCard = Number(readlineSync.question("Number: "));
-} while (creditCard < 100000000000 || creditCard > 9999999999999999 || Number.isNaN(creditCard) || !Number.isInteger(creditCard))
+  creditNumber = Number(readlineSync.question("Number: "));
+} while (creditNumber < 1000000000000 || creditNumber > 9999999999999999 || Number.isNaN(creditNumber) || !Number.isInteger(creditNumber))
 
-creditCard = String(creditCard)
-creditLength = creditCard.length
+creditNumber2 = creditNumber
 
-
-if (creditLength % 2 == 0) {
-  for (i = creditLength; i = 0; i - 2) {
-    var number = creditCard.charAt(creditLength-2)
-    sum = sum + number * 2
-  }
+while (creditNumber2 >= 1) {
+  creditNumber2 = creditNumber2 / 10
+  digit = digit + 1
 }
-else {
-  for (i = creditLength + 1; i = 0; i - 2) {
-    var number = creditCard.charAt(creditLength-2)
-    sum = sum + number * 2
+digit1 = Math.floor(creditNumber / 10 ** (digit - 1))
+digit2 = (Math.floor(creditNumber / 10 ** (digit - 2)) / 10) * 10
+
+for (let loop = 0; loop < digit; loop++) {
+  singularDigit = (creditNumber - Math.floor(creditNumber / 10) * 10)
+  creditNumber = Math.floor(creditNumber / 10)
+  if (loop % 2 == 1) {
+    if (singularDigit * 2 >= 10) {
+      addition = addition + ((singularDigit * 2 - singularDigit * 2 % 10) / 10 + singularDigit * 2 % 10)
+    }
+    else if (singularDigit * 2 < 10) {
+      addition = addition + singularDigit * 2
+    }
   }
+  else {
+    addition = addition + singularDigit
+  }
+  counter = counter + 1
 }
-console.log(sum);
+
+if (addition / 10 % 1 != 0) {
+  console.log("\nInvalid.")
+}
+else if (digit == 15 && (digit2 == 34 || digit2 == 37)) {
+  console.log("\nAmex.")
+}
+else if (digit == 16 && (digit2 <= 55 && digit2 >= 51)) {
+  console.log("\nMastercard.")
+}
+else if ((digit == 13 || digit == 16) && digit1 == 4) {
+  console.log("\nVisa.")
+}
